851a421e6ca02014556b8fa969fda34943177352
vzazueta <a00226702@itesm.mx>
2019-09-05 23:36:59 +0000

    Tarea2

--- xv6-public/syscall.c
+++ xv6-public/syscall.c
@@ -132,6 +132,32 @@ static int (*syscalls[])(void) = {
 [SYS_reboot]  sys_reboot,
 };
 
+static char* scprints[] = {
+[SYS_fork]    "fork",
+[SYS_exit]    "exit",
+[SYS_wait]    "wait",
+[SYS_pipe]    "pipe",
+[SYS_read]    "read",
+[SYS_kill]    "kill",
+[SYS_exec]    "exec",
+[SYS_fstat]   "fstat",
+[SYS_chdir]   "chdir",
+[SYS_dup]     "dup",
+[SYS_getpid]  "getpid",
+[SYS_sbrk]    "sbrk",
+[SYS_sleep]   "sleep",
+[SYS_uptime]  "uptime",
+[SYS_open]    "open",
+[SYS_write]   "write",
+[SYS_mknod]   "mknod",
+[SYS_unlink]  "unlink",
+[SYS_link]    "link",
+[SYS_mkdir]   "mkdir",
+[SYS_close]   "close",
+[SYS_shutdown] "shutdown",
+[SYS_reboot]  "reboot",
+};
+
 void
 syscall(void)
 {
@@ -140,7 +166,8 @@ syscall(void)
 
   num = curproc->tf->eax;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
-    curproc->tf->eax = syscalls[num]();
+        cprintf("Llamada: %s", scprints[num]);
+        curproc->tf->eax = syscalls[num]();
   } else {
     cprintf("%d %s: unknown sys call %d\n",
             curproc->pid, curproc->name, num);
